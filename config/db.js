dcd9a74e7c6955e8c67f9c261d4f96b6923b7a3e
Mission Shretha <missionshrestha99@gmail.com>
2023-05-09 17:59:04 +0545

    Server setup & mongodb connected

---
+++ config/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+
+import dotenv from 'dotenv';
+
+
+//dotenv config
+dotenv.config();
+
+const connectDB = async () => {
+
+    try {
+
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        // const conn = await mongoose.connect('mongodb+srv://admin-mission:passmission-MONGODB123@cluster0-missionshresth.tcwumzp.mongodb.net/pashupati-market');
+        console.log(`Connected To Mongodb Databsase ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error in MongoDB ${error}`.bgRed.white);
+    }
+}
+
+export default connectDB;
---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan'
+import connectDB from './config/db.js';
+
+
+
+
+//database config
+connectDB();
+
+//dotenv config
+dotenv.config();
+
+//rest object
+const app = express();
+
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+//rest api
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello</h1>");
+})
+
+//port 
+const PORT = process.env.PORT || 8080;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Sever running at ${process.env.DEV_MODE} on port ${PORT}`.bgCyan.white);
+})
+
