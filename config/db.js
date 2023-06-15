0454dde1b510d0999155d4a1edc5a8c49bb4f3b8
Mission Shretha <missionshrestha99@gmail.com>
2023-06-15 21:20:01 +0545

    mistaked corrected

--- config/db.js
+++ config/db.js
@@ -12,7 +12,6 @@ const connectDB = async () => {
     try {
 
         const conn = await mongoose.connect(process.env.MONGO_URL);
-        // const conn = await mongoose.connect('mongodb+srv://admin-mission:passmission-MONGODB123@cluster0-missionshresth.tcwumzp.mongodb.net/pashupati-market');
         console.log(`Connected To Mongodb Databsase ${conn.connection.host}`.bgMagenta .white)
     } catch (error) {
         console.log(`Error in MongoDB ${error}`.bgRed.white);
